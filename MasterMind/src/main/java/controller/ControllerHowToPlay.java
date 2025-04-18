b0172a505b9fe6bf0e896f9fdceecf81719f11b1
Nuria Calo <nuriacalomosquera@gmail.com>
2025-04-18 12:13:31 +0200

    Added HowToPlay view and functionality with navigation from Index

---
+++ MasterMind/src/main/java/controller/ControllerHowToPlay.java
@@ -0,0 +1,31 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import view.ViewHowToPlay;
+import view.ViewIndex;
+
+/**
+ *
+ * @author nuriacalo
+ */
+public class ControllerHowToPlay implements ActionListener {
+
+    private ViewHowToPlay view;
+    private ViewIndex viewIndex;
+
+    public ControllerHowToPlay(ViewHowToPlay view, ViewIndex viewIndex) {
+        this.view = view;
+        this.viewIndex = viewIndex;
+        this.view.setActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if ("back".equals(e.getActionCommand())) {
+            view.setVisible(false);
+            viewIndex.setVisible(true); // Volver ao Index
+        }
+    }
+}
